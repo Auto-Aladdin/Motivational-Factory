113848855cb813132fdc455d2c91f3de3b408d88
Auto-Aladdin <aladdinauto@atomicmail.io>
2026-09-17 12:53:41 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
-from cloudflare_ai import generate_motivation
+import json
+from cloudflare_ai import generate_short_plan
+
 
 if __name__ == "__main__":
-    result = generate_short_plan()
-    print(result)
+
+    result = generate_short_plan("discipline")
+
+    with open(
+        "../cache/short_plan.json",
+        "w",
+        encoding="utf-8"
+    ) as file:
+        json.dump(
+            result,
+            file,
+            indent=2,
+            ensure_ascii=False
+        )
+
+    print("Short plan generated successfully")
